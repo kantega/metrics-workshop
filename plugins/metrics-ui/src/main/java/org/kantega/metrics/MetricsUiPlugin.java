d65df2e1d97fc2250bf55c0cd2f57d34b74f9834
Espen Fossen <espenaf@junta.no>
2015-02-13 14:39:31 +0100

    Updated metric-ui to use reststop-webjars-plugin.

--- plugins/metrics-ui/src/main/java/org/kantega/metrics/MetricsUiPlugin.java
+++ plugins/metrics-ui/src/main/java/org/kantega/metrics/MetricsUiPlugin.java
@@ -4,26 +4,23 @@ package org.kantega.metrics;
 import org.apache.commons.io.IOUtils;
 import org.kantega.reststop.api.DefaultReststopPlugin;
 import org.kantega.reststop.api.Reststop;
+import org.kantega.reststop.webjars.WebjarsVersions;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.util.*;
+import java.util.Map;
 
 public class MetricsUiPlugin extends DefaultReststopPlugin {
 
-    public MetricsUiPlugin(Reststop reststop) {
-
-        final Map<String, String> versions = getVersionsForWebJars();
+    public MetricsUiPlugin(Reststop reststop, WebjarsVersions webjarsVersions) {
 
         addServletFilter(reststop.createServletFilter(new HttpServlet() {
             @Override
             protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                final Map<String, String> versions = webjarsVersions.getVersions();
                 resp.setContentType("text/html");
                 String html = IOUtils.toString(getClass().getResourceAsStream("index.html"), "utf-8");
                 for (String version : versions.keySet()) {
@@ -34,57 +31,7 @@ public class MetricsUiPlugin extends DefaultReststopPlugin {
                 IOUtils.write(html, resp.getOutputStream());
 
             }
-        }, "/"));
-        addServletFilter(reststop.createServletFilter(new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                String filename = req.getRequestURI().substring(req.getRequestURI().lastIndexOf("/") + 1);
-                resp.setContentType(req.getServletContext().getMimeType(filename));
-                String path = req.getRequestURI().substring(req.getContextPath().length() + req.getServletPath().length());
-                InputStream resourceAsStream = getClass().getResourceAsStream("/META-INF/resources/webjars" + path);
-                if (resourceAsStream == null) {
-                    resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-                } else {
-                    IOUtils.copy(resourceAsStream, resp.getOutputStream());
-                }
-            }
-        }, "/webjars/*"));
-
-    }
-
-    private Map<String, String> getVersionsForWebJars() {
-        HashMap<String, String> versions = new HashMap<>();
-
-        Set<String> webjars = new HashSet<>();
-
-        try {
-            Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/resources/webjars/");
-            while (resources.hasMoreElements()) {
-                URL webJar = resources.nextElement();
-                String file = URLDecoder.decode(webJar.getFile(), "UTF-8");
-                file = file.substring(0, file.indexOf("!"));
-                webjars.add(file);
-
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        for (String webjar : webjars) {
-            String file = webjar.substring(0, webjar.lastIndexOf("/"));
-
-            String version = file.substring(file.lastIndexOf("/") + 1);
-            String artifact = file.substring(0, file.lastIndexOf("/"));
-            String artifactId = artifact.substring(artifact.lastIndexOf("/") + 1);
-
-            if (version.contains("-")) {
-                version = version.substring(0, version.lastIndexOf("-"));
-            }
-
-            versions.put("versions." + artifactId, version);
-        }
-
-        return versions;
+        }, "/metrics-ui/"));
 
     }
 }
