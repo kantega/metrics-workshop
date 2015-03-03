03b35e6825d2302bbc0d76b3e045325477c310be
Espen Fossen <espenaf@junta.no>
2015-03-03 13:57:21 +0100

    Simplified generator to simulate number of users.

--- plugins/load-ui/src/main/resources/assets/loadui/loadui.js
+++ plugins/load-ui/src/main/resources/assets/loadui/loadui.js
@@ -2,15 +2,27 @@ var loadUiApp = angular.module('loadUiApp',  []);
 
 loadUiApp.controller('loadCtrl', ['$scope', '$http','$interval', function ($scope, $http, $interval) {
 
-        $scope.myInterval = 500;
-        $scope.myIterations = 10;
-        $scope.myUrl = '/helloworld';
+        $scope.myInterval = 1000;
+        $scope.myUsers = 1;
+        $scope.myUrl = '/#/blogs';
+        $scope.running = false;
 
         var stop;
-        $scope.start = function(myInterval, myIterations, url) {
+        $scope.start = function(myUsers, url) {
             if ( angular.isDefined(stop) ) return;
-            $scope.runningIterations = myIterations;
-            stop = $interval(function(){ $scope.startGetRequest(url); }, myInterval);
+            $scope.running = true;
+            var interval = Math.floor($scope.myInterval/myUsers);
+            stop = $interval(function(){ $scope.startGetRequest(url); }, interval);
+        };
+
+        $scope.addUser = function() {
+            $scope.myUsers++;
+        };
+
+        $scope.subUser = function() {
+            if($scope.myUsers != 0){
+                $scope.myUsers--;
+            }
         };
 
         $scope.stop = function() {
@@ -18,16 +30,12 @@ loadUiApp.controller('loadCtrl', ['$scope', '$http','$interval', function ($scop
                 $interval.cancel(stop);
                 stop = undefined;
             }
+            $scope.running = false;
         };
 
         $scope.startGetRequest = function(url){
-            if($scope.runningIterations != 0){
-                $scope.runningIterations = $scope.runningIterations - 1;
                 $http.get(url);
                 console.log ('Http Get request to '+ url);
-            } else {
-                $scope.stop();
-            }
         };
 
         $scope.$on('$destroy', function() {
