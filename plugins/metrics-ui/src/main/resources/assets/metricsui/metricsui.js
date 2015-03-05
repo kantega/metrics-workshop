76e16def28a85c2d4f2d5f7e862184f515ccb78e
Espen Fossen <espenaf@junta.no>
2015-03-05 13:48:00 +0100

    Added Guage example.

--- plugins/metrics-ui/src/main/resources/assets/metricsui/metricsui.js
+++ plugins/metrics-ui/src/main/resources/assets/metricsui/metricsui.js
@@ -1,34 +1,77 @@
-var metricsUiApp = angular.module('metricsUiApp',  [
+var metricsUiApp = angular.module('metricsUiApp', [
     "ngRoute",
     "highcharts-ng"
 ]);
 
 metricsUiApp.controller('chartController', ['$scope', '$http', function ($scope, $http) {
 
-    var chartData = [];
+    $scope.staticChartData = [10, 15, 12, 8, 7, 10, 20, 19, 5, 22, 30, 15, 10];
 
-    $http.get('/metrics/')
-        .success(function(jsonData){
-            // Do some remodeling of the jsonData if necessary, else just give it to the chart:
-            chartData = jsonData;
-        });
-
-
-    $scope.chartConfig = {
+    $scope.chart1Config = {
         options: {
             chart: {
-                type: 'column'
+                type: 'line'
             }
         },
         series: [{
-            data: [10, 15, 12, 8, 7]
+            data: $scope.staticChartData
         }],
         title: {
-            text: 'HelloWorld'
+            text: 'Chart 1'
         },
 
         loading: false
-    }
+    };
+
+    $http.get('/metrics/')
+        .success(function (jsonData) {
+            // Do some remodeling of the jsonData if necessary, else just give it to the chart:
+            $scope.heapMax = jsonData.gauges["heap.max"].value;
+            $scope.heapMax2 = Math.floor($scope.heapMax/2);
+            $scope.heapMax3 = Math.floor($scope.heapMax/1.5);
+            $scope.heapUsed = [jsonData.gauges["heap.used"].value];
+
+            $scope.chart2Config = {
+                options: {
+                    chart: {
+                        type: 'gauge'
+                    },
+
+                    pane: {
+                        startAngle: -150,
+                        endAngle: 150
+                    }
+
+                },
+                series: [{
+                    name: 'Heap Usage',
+                    data: $scope.heapUsed
+                }],
+                yAxis: [{
+                    min: 0,
+                    max: $scope.heapMax,
+
+                    plotBands: [{
+                        from: 0,
+                        to: $scope.heapMax2,
+                        color: '#55BF3B' // green
+                    }, {
+                        from: $scope.heapMax2,
+                        to: $scope.heapMax3,
+                        color: '#DDDF0D' // yellow
+                    }, {
+                        from: $scope.heapMax3,
+                        to: $scope.heapMax,
+                        color: '#DF5353' // red
+                    }]
+                }],
+                title: {
+                    text: 'Chart 2'
+                }
+
+            }
+
+        });
 
 
 }]);
