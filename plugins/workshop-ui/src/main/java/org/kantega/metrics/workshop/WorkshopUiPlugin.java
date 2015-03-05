3761510dc1c5f48d1fda233061a3c379d2d6ac6f
Eirik Bjørsnøs <eirbjo@gmail.com>
2015-03-05 14:39:40 +0100

    Getting started with tests

--- plugins/workshop-ui/src/main/java/org/kantega/metrics/workshop/WorkshopUiPlugin.java
+++ plugins/workshop-ui/src/main/java/org/kantega/metrics/workshop/WorkshopUiPlugin.java
@@ -1,7 +1,6 @@
 
 package org.kantega.metrics.workshop;
 
-import org.apache.commons.io.IOUtils;
 import org.kantega.metrics.api.templates.TemplateRenderer;
 import org.kantega.reststop.api.DefaultReststopPlugin;
 import org.kantega.reststop.api.Reststop;
@@ -17,31 +16,42 @@ import java.util.Map;
 
 public class WorkshopUiPlugin extends DefaultReststopPlugin {
 
+    private final Reststop reststop;
+    private final WebjarsVersions webjarsVersions;
+    private final TemplateRenderer templateRenderer;
+
     public WorkshopUiPlugin(Reststop reststop, WebjarsVersions webjarsVersions, TemplateRenderer templateRenderer) {
+        this.reststop = reststop;
+        this.webjarsVersions = webjarsVersions;
+        this.templateRenderer = templateRenderer;
 
-        addServletFilter(reststop.createServletFilter(new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                final Map<String, String> versions = webjarsVersions.getVersions();
-                resp.setContentType("text/html");
-                if(false) {
-                    String html = IOUtils.toString(getClass().getResourceAsStream("index.html"), "utf-8");
-                    for (String version : versions.keySet()) {
-                        String prop = "${" + version + "}";
-                        html = html.replace(prop, versions.get(version));
-                    }
-
-                    IOUtils.write(html, resp.getOutputStream());
-                } else {
-                    TemplateRenderer.Renderer renderer = templateRenderer
-                            .template("org/kantega/metrics/workshop/index.html", Charset.forName("utf-8"));
-                    versions.entrySet().forEach((e) -> renderer.addAttribute(e.getKey().replace('.','_'), e.getValue()));
-                    renderer.render(resp.getWriter());
-                }
-
-            }
-        }, "/workshop-ui/"));
+        templateServlet("index.html", "/workshop-ui/");
+        templateServlet("gettingstarted.html", "/workshop-ui/gettingstarted");
 
 
+    }
+
+    private void templateServlet(String file, String path) {
+        addServletFilter(reststop.createServletFilter(new TemplateServlet(file), path));
+    }
+
+    class TemplateServlet extends HttpServlet {
+
+        private final String file;
+
+        TemplateServlet(String file) {
+            this.file = file;
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            final Map<String, String> versions = webjarsVersions.getVersions();
+            resp.setContentType("text/html");
+            TemplateRenderer.Renderer renderer = templateRenderer
+                    .template("org/kantega/metrics/workshop/" + file, Charset.forName("utf-8"));
+            versions.entrySet().forEach((e) -> renderer.addAttribute(e.getKey().replace('.', '_'), e.getValue()));
+            renderer.render(resp.getWriter());
+
+        }
     }
 }
