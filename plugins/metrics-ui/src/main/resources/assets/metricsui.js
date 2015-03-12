9f7cf1aad2b6462870187c34c3a4db9a56a86a83
Espen Fossen <espenaf@junta.no>
2015-03-12 07:23:10 +0100

    Fixes

--- plugins/metrics-ui/src/main/resources/assets/metricsui.js
+++ plugins/metrics-ui/src/main/resources/assets/metricsui.js
@@ -5,7 +5,7 @@ var metricsUiApp = angular.module('metricsUiApp', [
 
 metricsUiApp.controller('chartController', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.staticChartData = [10, 15, 12, 8, 7, 10, 20, 19, 5, 22, 30, 15, 10];
+    var staticChartData = [10, 15, 12, 8, 7, 10, 20, 19, 5, 22, 30, 15, 10];
 
     $scope.chart1Config = {
         options: {
@@ -14,7 +14,7 @@ metricsUiApp.controller('chartController', ['$scope', '$http', function ($scope,
             }
         },
         series: [{
-            data: $scope.staticChartData
+            data: staticChartData
         }],
         title: {
             text: 'Chart 1'
