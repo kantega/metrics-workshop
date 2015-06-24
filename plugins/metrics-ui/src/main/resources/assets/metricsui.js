01160b299369d24ed2a0891b4c843952661d3e47
Espen Fossen <espenaf@junta.no>
2015-06-24 14:50:28 +0200

    Added basic setup for metrics-graphics instead of Hightcharts. Tests not working yet.

--- plugins/metrics-ui/src/main/resources/assets/metricsui.js
+++ plugins/metrics-ui/src/main/resources/assets/metricsui.js
@@ -1,25 +1,14 @@
-var metricsUiApp = angular.module('metricsUiApp', [
-    "ngRoute",
-    "highcharts-ng"
-]);
-
-metricsUiApp.controller('chartController', ['$scope', '$http', function ($scope, $http) {
-
-    var staticChartData = [10, 15, 12, 8, 7, 10, 20, 19, 5, 22, 30, 15, 10];
-
-    $scope.chart1Config = {
-        options: {
-            chart: {
-                type: 'line'
-            }
-        },
-        series: [{
-            data: staticChartData
-        }],
-        title: {
-            text: 'Chart 1'
-        }
-
-    };
-
-}]);
+MG.data_graphic({
+    title: "Chart 1",
+    data: [{
+        'date': new Date('2014-10-28'),
+        'value': 7
+    }, {
+        'date': new Date('2014-11-01'),
+        'value': 12
+    }, {
+        'date': new Date('2014-11-02'),
+        'value': 18
+    }],
+    target: '#memory'
+});
